b7f5c4170ac5d12f6bc65fbb61a1fa9bbdb55d04
fuchse-ohren <git@kitsune.aleeas.com>
2026-04-18 10:54:24 +0900

    Cloudflare Workers の初期コメントを削除し、KV名前空間をコメントアウト、CORS レスポンスを修正

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 const CORSHEADERS = {
 	'Access-Control-Allow-Origin': process.env.CORS_DOMAIN || '127.0.0.1:8787',
 	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
@@ -88,7 +75,7 @@ export default {
 
 		// CORS対応
 		if (method === 'OPTIONS') {
-			return secureResponse(204, CORSHEADERS, null);
+			return secureResponse(204, CORSHEADERS, '');
 		}
 
 		// リクエストのオリジンを検証
